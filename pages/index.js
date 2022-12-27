7812369c0245f1d99a4366ad6f4e79fc10170159
Chin Ying Kong <chinYingAdora>
2022-12-27 18:05:23 +1100

    Navbar basic

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs';
 import { Inter } from '@next/font/google' // might need to delete as we have our own fonts
 
 const inter = Inter({ subsets: ['latin'] }) // same as Inter
@@ -13,9 +14,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">developedbyAdora</h1>
+            <ul className="flex items-center">
+              <li><BsFillMoonStarsFill className="cursor-pointer text-2xl" /></li>
+              <li><a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href='#'>Resume</a></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
         </section>
       </main>
     </>
