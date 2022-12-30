365e0087eb0ad99af791d855a6d4cbda48431d4d
Chin Ying Kong <chinYingAdora>
2022-12-31 00:15:17 +1100

    Updated themed Pink color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        themedPink: "rgb(251, 37, 118)",
+        themedPink: "#EF2F88",
         themedOrange: "rgb(255, 112, 0)",
         themedPurple: "#a855f7"        
+      },
+      boxShadow: {
+        'lightRounded': '0 0 18px #f5d0fe'
       }
     },
     fontFamily: {
