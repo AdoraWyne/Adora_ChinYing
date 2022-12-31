95517c9d156e4de1dfb5f41584f6b12bf3806f09
Chin Ying Kong <chinYingAdora>
2022-12-31 14:09:59 +1100

    Updated background color to the html root

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-themedPink">
+      <div>
         <HomePage />
         <AboutMe />
         <Skills />
