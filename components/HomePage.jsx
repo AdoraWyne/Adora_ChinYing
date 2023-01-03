710221f5e744641ceca434aa5727f8d50c6e99c4
Chin Ying Kong <chinYingAdora>
2023-01-03 12:34:53 +1100

    Finalised Homepage

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -6,10 +6,23 @@ const HomePage = () => {
     return (
         <div id="home" className="h-screen w-full text-center">
             <div className="max-w-screen-xl mx-auto w-full h-full pt-24 p-4 flex flex-col justify-around items-center">
-                <h1 className="font-bold text-white text-7xl">Full Stack Developer</h1>
+                {/* <h1 className="font-bold text-white text-7xl font-logo">Welcome to my space</h1>
                 <p className="text-white text-md max-w-sm mx-auto">
-                    Spicy jalapeno bacon ipsum dolor amet biltong capicola pastrami tenderloin, beef ribs spare ribs kielbasa pork bacon ribeye pork belly. Porchetta drumstick kielbasa alcatra. Meatloaf kielbasa biltong pancetta meatball ground round. 
+                    I love a little bit of cooking, a little bit of electronic music, a little bit of running and a little bit of coding! 👩🏻‍💻
+                </p> */}
+
+                <h1 className="font-bold text-white text-7xl font-logo">Welcome to my space</h1>
+
+                <p className="text-white text-lg max-w-sm mx-auto">
+                    Hi, I&apos;m a software engineer based in Melbourne with a passion for building delightful digital experiences.
                 </p>
+
+                <Link href='/#getintouch'>
+                    <div className="group flex items-center justify-center my-8 bg-fuchsia-100 text-themedPink px-6 py-3 font-bold border rounded-md hover:border-themedPink hover:scale-110 shadow-lightRounded">
+                        get in touch<span className="-rotate-90 duration-100 ease-in group-hover:rotate-0"><MdExpandMore size={25} /></span>
+                    </div>
+                </Link>
+
 
                 {/* <div className="flex items-center justify-between my-5">
                     <Link href='/#aboutme'>
